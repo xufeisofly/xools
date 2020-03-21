9a3f394832080ab4d9194156bb3d197c50f94c96
xufei01 <xufei01@zhihu.com>
2020-03-21 23:43:08 +0800

    benchmark

--- go/multimerge/example/main.go
+++ go/multimerge/example/main.go
@@ -3,61 +3,108 @@ package main
 import (
 	"fmt"
 	"go/go/multimerge"
+	"math/rand"
+	"sort"
 )
 
 type Item struct {
-	Value int
+	ID   int
+	Heat int
 }
 
 func (i Item) Equal(i2 multimerge.Noder) bool {
-	return i.Value == i2.(Item).Value
+	return i.ID == i2.(Item).ID
 }
 
 func (i Item) LessThan(i2 multimerge.Noder) bool {
-	return i.Value < i2.(Item).Value
+	return i.Heat < i2.(Item).Heat
 }
 
+type byItemIndex []Item
+
+func (a byItemIndex) Len() int           { return len(a) }
+func (a byItemIndex) Swap(i, j int)      { a[i], a[j] = a[j], a[i] }
+func (a byItemIndex) Less(i, j int) bool { return a[i].Heat > a[j].Heat }
+
 func main() {
+	fmt.Println("============ first example ==========")
 	l1 := []Item{
-		{Value: 30},
-		{Value: 25},
-		{Value: 20},
-		{Value: 18},
-		{Value: 17},
-		{Value: 14},
-		{Value: 12},
-		{Value: 11},
+		{ID: 1, Heat: 100},
+		{ID: 2, Heat: 98},
+		{ID: 3, Heat: 97},
+		{ID: 4, Heat: 95},
+		{ID: 5, Heat: 80},
 	}
 
 	l2 := []Item{
-		{Value: 33},
-		{Value: 24},
-		{Value: 22},
-		{Value: 19},
-		{Value: 16},
-		{Value: 15},
-		{Value: 13},
-		{Value: 10},
+		{ID: 6, Heat: 100},
+		{ID: 7, Heat: 97},
+		{ID: 8, Heat: 93},
+		{ID: 9, Heat: 92},
+		{ID: 10, Heat: 81},
 	}
 
 	l3 := []Item{
-		{Value: 50},
-		{Value: 36},
-		{Value: 31},
-		{Value: 17},
-		{Value: 15},
-		{Value: 12},
-		{Value: 11},
-		{Value: 9},
+		{ID: 11, Heat: 99},
+		{ID: 12, Heat: 93},
+		{ID: 13, Heat: 92},
+		{ID: 14, Heat: 90},
+		{ID: 15, Heat: 88},
 	}
 
-	lb := [][]Item{l1, l2, l3}
+	l4 := []Item{
+		{ID: 16, Heat: 94},
+		{ID: 17, Heat: 92},
+		{ID: 18, Heat: 92},
+		{ID: 19, Heat: 87},
+		{ID: 20, Heat: 85},
+	}
+
+	l5 := []Item{
+		{ID: 21, Heat: 95},
+		{ID: 22, Heat: 91},
+		{ID: 23, Heat: 89},
+		{ID: 24, Heat: 88},
+		{ID: 25, Heat: 83},
+	}
+
+	lb := [][]Item{l1, l2, l3, l4, l5}
 
 	ms := multimerge.NewSort(lb)
-	nodes := ms.TopK(10)
+	nodes := ms.TopK(8)
 
 	for _, node := range nodes {
 		item := node.(Item)
-		fmt.Println(item.Value)
+		fmt.Println(item)
 	}
+
+	fmt.Println("============ second example ==========")
+
+	L := 100000
+	l := make([]Item, L)
+
+	for i := 0; i < L; i++ {
+		l[i] = Item{Heat: rand.Intn(L), ID: i}
+	}
+
+	ll := make([]Item, L)
+
+	for i := 0; i < L; i++ {
+		ll[i] = Item{Heat: rand.Intn(L), ID: i + L + 1}
+	}
+
+	lll := make([]Item, L)
+
+	for i := 0; i < L; i++ {
+		lll[i] = Item{Heat: rand.Intn(L), ID: i + 2*L + 1}
+	}
+
+	sort.Sort(byItemIndex(l))
+	sort.Sort(byItemIndex(ll))
+	sort.Sort(byItemIndex(lll))
+
+	lb2 := [][]Item{l, ll, lll}
+	mss := multimerge.NewSort(lb2)
+	nodes = mss.TopK(L)
+	fmt.Println(nodes)
 }
--- go/multimerge/multimerge_test.go
+++ go/multimerge/multimerge_test.go
@@ -2,63 +2,74 @@ package multimerge_test
 
 import (
 	"go/go/multimerge"
+	"math/rand"
+	"os"
+	"sort"
 	"testing"
 )
 
 type Item struct {
-	Value int
+	ID   int
+	Heat int
 }
 
 func (i Item) Equal(i2 multimerge.Noder) bool {
-	return i.Value == i2.(Item).Value
+	return i.ID == i2.(Item).ID
 }
 
 func (i Item) LessThan(i2 multimerge.Noder) bool {
-	return i.Value < i2.(Item).Value
+	return i.Heat < i2.(Item).Heat
 }
 
 func TestTopK(t *testing.T) {
 	l1 := []Item{
-		{Value: 30},
-		{Value: 25},
-		{Value: 20},
-		{Value: 18},
-		{Value: 17},
-		{Value: 14},
-		{Value: 12},
-		{Value: 11},
+		{ID: 1, Heat: 100},
+		{ID: 2, Heat: 98},
+		{ID: 3, Heat: 97},
+		{ID: 4, Heat: 95},
+		{ID: 5, Heat: 80},
 	}
 
 	l2 := []Item{
-		{Value: 33},
-		{Value: 24},
-		{Value: 22},
-		{Value: 19},
-		{Value: 16},
-		{Value: 15},
-		{Value: 13},
-		{Value: 10},
+		{ID: 6, Heat: 100},
+		{ID: 7, Heat: 97},
+		{ID: 8, Heat: 93},
+		{ID: 9, Heat: 92},
+		{ID: 10, Heat: 81},
 	}
 
 	l3 := []Item{
-		{Value: 50},
-		{Value: 36},
-		{Value: 31},
-		{Value: 17},
-		{Value: 15},
-		{Value: 12},
-		{Value: 11},
-		{Value: 9},
+		{ID: 11, Heat: 99},
+		{ID: 12, Heat: 93},
+		{ID: 13, Heat: 92},
+		{ID: 14, Heat: 90},
+		{ID: 15, Heat: 88},
 	}
 
-	expected := []int{50, 36, 33, 31, 30}
+	l4 := []Item{
+		{ID: 16, Heat: 94},
+		{ID: 17, Heat: 92},
+		{ID: 18, Heat: 92},
+		{ID: 19, Heat: 87},
+		{ID: 20, Heat: 85},
+	}
 
-	ms := multimerge.NewSort([][]Item{l1, l2, l3})
+	l5 := []Item{
+		{ID: 21, Heat: 95},
+		{ID: 22, Heat: 91},
+		{ID: 23, Heat: 89},
+		{ID: 24, Heat: 88},
+		{ID: 25, Heat: 83},
+	}
+
+	expected := []int{100, 100, 99, 98, 97}
+
+	ms := multimerge.NewSort([][]Item{l1, l2, l3, l4, l5})
 	var values = make([]int, 5)
 	rets := ms.TopK(5)
 
 	for i, ret := range rets {
-		values[i] = ret.(Item).Value
+		values[i] = ret.(Item).Heat
 	}
 
 	for i, value := range values {
@@ -70,4 +81,60 @@ func TestTopK(t *testing.T) {
 	if len(values) != len(expected) {
 		t.Fatalf("TopK is not right, expected: %v, actual: %v", expected, values)
 	}
+}
+
+type byItemIndex []Item
+
+func (a byItemIndex) Len() int           { return len(a) }
+func (a byItemIndex) Swap(i, j int)      { a[i], a[j] = a[j], a[i] }
+func (a byItemIndex) Less(i, j int) bool { return a[i].Heat > a[j].Heat }
+
+const L = 100000
+const numList = 50
+const K = 100
+
+func getLists(L int) [][]Item {
+	var listBundle = make([][]Item, numList)
+
+	for i := 0; i < numList; i++ {
+		l := make([]Item, L)
+
+		for j := 0; j < L; j++ {
+			l[j] = Item{Heat: rand.Intn(L), ID: j + i*(L+1)}
+		}
+
+		sort.Sort(byItemIndex(l))
+		listBundle[i] = l
+	}
+
+	return listBundle
+}
+
+// BenchmarkTopK-4      	       1	1522500513 ns/op
+func BenchmarkTopK(b *testing.B) {
+	lb := getLists(L)
+
+	for i := 0; i < b.N; i++ {
+		ms := multimerge.NewSort(lb)
+		_ = ms.TopK(K)
+	}
+}
+
+// BenchmarkOldTopK-4   	       1	1981787002 ns/op
+func BenchmarkOldTopK(b *testing.B) {
+	lb := getLists(L)
+
+	for i := 0; i < b.N; i++ {
+		var lall = make([]Item, len(lb)*L)
+		for _, l := range lb {
+			lall = append(lall, l...)
+		}
+
+		sort.Sort(byItemIndex(lall))
+		_ = lall[:K]
+	}
+}
+
+func TestMain(m *testing.M) {
+	os.Exit(m.Run())
 }
